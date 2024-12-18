34d094bff01e197e6862621d959af3af7ee7c60b
yihune21 <zewdieyihune@gmail.com>
2024-12-18 11:29:05 +0300

    create Event model for calendar service microservice.

--- calendar_service/calendar_service/events/models.py
+++ calendar_service/calendar_service/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
