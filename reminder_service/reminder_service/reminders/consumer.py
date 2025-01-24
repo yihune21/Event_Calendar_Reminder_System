e06088a80fbafc6fd8b0106f63571c988f40d80b
Yeabsira Yonas <yabulala432@gmail.com>
2025-01-24 10:51:21 +0300

    consumer

---
+++ reminder_service/reminder_service/reminders/consumer.py
@@ -0,0 +1,53 @@
+# import pika
+
+# class Consumer:
+#     def __init__(self, queue_name='event_queue', host='localhost'):
+#         self.queue_name = queue_name
+#         self.host = host
+#         self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
+#         self.channel = self.connection.channel()
+#         self.channel.queue_declare(queue=self.queue_name)
+
+#     def callback(self, ch, method, properties, body):
+#         print(f" [x] Received {body}")
+
+#     def start_consuming(self):
+#         self.channel.basic_consume(queue=self.queue_name,
+#                                    on_message_callback=self.callback,
+#                                    auto_ack=True)
+#         print(' [*] Waiting for messages. To exit press CTRL+C')
+#         self.channel.start_consuming()
+
+# if __name__ == "__main__":
+#     consumer = Consumer()
+#     consumer.start_consuming()
+
+
+import pika
+import threading
+
+class Consumer:
+    def __init__(self, queue_name='event_queue', host='localhost'):
+        self.queue_name = queue_name
+        self.host = host
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+
+    def callback(self, ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    def start_consuming(self):
+        self.channel.basic_consume(queue=self.queue_name,
+                                   on_message_callback=self.callback,
+                                   auto_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel.start_consuming()
+
+    def run(self):
+        consuming_thread = threading.Thread(target=self.start_consuming)
+        consuming_thread.start()
+
+if __name__ == "__main__":
+    consumer = Consumer()
+    consumer.run()
